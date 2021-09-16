4aa457812ba09a7a88fdba5a1ba7b8da6f4fbc28
Kyle Sanders <76092420+kylesanders010@users.noreply.github.com>
2021-09-15 19:27:49 -0700

    add retractable mobile sidebar

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const elbuttonArray = document.getElementsByClassName('mobile');
+const elsideBarArray = document.getElementsByClassName('sidebar');
+
 const button = elbuttonArray[0];
+const sidebar = elsideBarArray[0];
 
 let flipped = false;
 
 function triggerMenuButton(event) {
     if (flipped === false) {
+        
         button.style.transform = 'rotate(90deg)';
+        sidebar.style.transform = 'translate(67%)';
         flipped = true;
     } else {
         button.style.transform = 'rotate(0deg)';
+        sidebar.style.transform = 'translate(167%)';
         flipped = false;
     }
     
     
     
 }
-
 button.addEventListener('click', triggerMenuButton);
